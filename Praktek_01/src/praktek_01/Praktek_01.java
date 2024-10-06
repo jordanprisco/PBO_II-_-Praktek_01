e4e491b3a664784f7e20c4b13e730b4fbc28838f
AEP CAHYANA <AEP CAHYANA@DESKTOP-8UGG1V7>
2024-10-06 20:45:22 +0700

    first Commit

---
+++ Praktek_01/src/praktek_01/Praktek_01.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package praktek_01;
+
+import com.mysql.jdbc.Driver;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Praktek_01 {
+     static final String DB_Url = "jdbc:mysql://localhost:3306/db_unidha";
+    private static Connection Test_Konek;
+    public static Connection DB_Konek() throws SQLException,ClassNotFoundException {
+        try{
+            Driver myDriver = new com.mysql.jdbc.Driver();
+            DriverManager.registerDriver(myDriver);
+            System.out.println("Proses Deteksi Driver Berhasil");
+            
+            final java.sql.Connection tersambung = DriverManager.getConnection(DB_Url,"root","");
+        } catch (final SQLException ex){
+            System.out.println("Koneksi Database Gagal");
+        }
+       return Test_Konek;
+       }
+    public static void main(String[] args) {
+       
+    }
+    
+    
+}
